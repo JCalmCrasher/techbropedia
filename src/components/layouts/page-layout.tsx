9f8cf146d11870cdf866c1353af1d3f59ccfe956
JoshX <joshx.dev@gmail.com>
2023-08-02 06:43:25 +0100

    misc

--- src/components/layouts/page-layout.tsx
+++ src/components/layouts/page-layout.tsx
@@ -1,20 +1,22 @@
+"use client";
+
 import { ReactNode, useEffect, useState } from "react";
 import TheFooter from "./footer";
 import TheHeader from "./header";
 
 export default function PageLayout({ children }: { children: ReactNode }) {
-  const [loaded, setLoaded] = useState(false);
+ const [loaded, setLoaded] = useState(false);
 
-  useEffect(() => {
-    setLoaded(true);
-  }, [setLoaded]);
+ useEffect(() => {
+  setLoaded(true);
+ }, [setLoaded]);
 
-  if (!loaded) return <></>;
-  return (
-    <>
-      <TheHeader />
-      <main>{children}</main>
-      <TheFooter />
-    </>
-  );
+ if (!loaded) return <></>;
+ return (
+  <>
+   <TheHeader />
+   <main>{children}</main>
+   <TheFooter />
+  </>
+ );
 }
