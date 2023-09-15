17392d3b05f9516a5bd4475bb3928bc763128426
JoshX <joshx.dev@gmail.com>
2023-09-15 04:17:31 +0100

    config: allow any `hostname` with `https`

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["www.benjamindada.com", "techpoint.africa"]
-  }
+ reactStrictMode: true,
+ images: {
+  // domains: ["www.benjamindada.com", "techpoint.africa"]
+  remotePatterns: [
+   {
+    protocol: "https",
+    hostname: "**"
+   }
+  ]
+ }
 };
 
 module.exports = nextConfig;
