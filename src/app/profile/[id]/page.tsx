02e718394fe82c346e7f91682c64f76a4ed1d7c3
JoshX <joshx.dev@gmail.com>
2023-10-21 06:48:38 +0100

    Update page.tsx

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -4,11 +4,11 @@ import type { Params } from "@/types";
 
 export default function ProfilePage({ params: { id } }: Params) {
  return (
-  <Container>
+  <div className="px-x-sm tablet:px-x-md laptop:px-x-lg">
    <h1 className="capitalize text-headline-sm">{unSlugify(id)}</h1>
    <div className="mt-4 mb-auto h-screen">
     <p>Working on this section. Please check back later.</p>
    </div>
-  </Container>
+  </div>
  );
 }
